fc1551564012f518319e238f59cbf9d6d7f8fd06
ofekmatan <CashregisterProject@gmail.com>
2022-06-11 13:01:54 +0300

    Added authorization to user

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 // import routes
 const authRoutes = require("./routes/auth");
@@ -10,6 +11,7 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.get("/api", (req, res) => {
     res.send("Fullstack React Course");
